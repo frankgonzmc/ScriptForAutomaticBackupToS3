9c5220a180edb39505a15073b244ee85c2a82fcd
Frank Puertas <frankgzmc@gmail.com>
2025-08-01 09:51:46 -0500

    Add iam role to ec2 resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,16 +68,53 @@ resource "aws_security_group" "ec2_sg"{
     }
 }
 
+#IAM ROLE AND ACCESS FROM EC2
+resource "aws_iam_role" "ec2_role"{
+    name = "ec2_backup_role"
+    assume_role_policy = jsondecode ({
+        Version = "2012-10.17"
+        Statement = [{
+            Effect = "Allow",
+            Principal = {
+                Service = "ec2.amazonaws.com"
+            },
+            Action = "sts:AssumeRole"
+        }]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access"{
+    role = aws_iam_role.ec2_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile"{
+    name = "ec2_backup_instance_profile"
+    role = aws_iam_role.ec2_role.name
+}
+###
+
 resource "aws_instance" "instance" {
     ami = "ami-020cba7c55df1f615" #Ubuntu
     instance_type = "t2.micro"
     subnet_id = aws_subnet.subnet_public_myvpc.id
     vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+    iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
     user_data = <<-EOF
                     !#/bin/bash
                     sudo apt-get update -y
                     mkdir -p cont
                     echo "Esto es un ejemplo" > /home/ubuntu/cont/file.txt
+                    echo "Esto es un ejemplo2" > /home/ubuntu/cont/file2.txt
+                    echo "Esto es un ejemplo3" > /home/ubuntu/cont/file3.txt
+                    #Create the script of the backup from terraform
+                    cat > /home/ubuntu/backup.sh << 'EOL'
+                    ${file("${path.module}/script.sh")}
+                    EOL
+                    #Give permissions
+                    chmod +x /home/ubuntu/backup.sh
+                    #Configure cron to execute the backup every 2 minutes
+                    (crontab -l 2>/dev/null; echo "*/2 * * * * /home/ubuntu/backup.sh") | crontab -
                 EOF
     key_name = aws_key_pair.mykey.key_name
     tags = {
